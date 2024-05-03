bc39e398dc1ca3b7e6a07277c764bb4e528cf5b6
Salim-Hubs <mohamed.ceddah@etu.univ-amu.fr>
2024-05-03 14:33:35 +0200

    a

--- Main.cpp
+++ Main.cpp
@@ -27,57 +27,42 @@ Item * new_item(int largeur, int hauteur){
   return i;
 }
 
-void astar(){
-
-// je pars d'une position et mon astar doit me donner toute les cases atteigables depuis cette postion 
-
+std::vector<Item*> astar(int x, int y, int id) {
+    std::vector<Item*> reachableNodes;
     Item *cur_node, *child_p, *temp;
 
-
-
-    while (listCount(&openList_p) != 0){ 
+    while (listCount(&openList_p) != 0) { 
         /* Get the first item on the open list */
-        cur_node = popBest(&openList_p) ;
+        cur_node = popBest(&openList_p);
 
+        /* Add current node to reachable nodes */
+        reachableNodes.push_back(cur_node);
 
-
-		/* Do we have a solution? */
-        if (evaluateBoard(cur_node) == 0 ){
-            showSolution(cur_node);
-            return;
+        if (onList(&closedList_p, cur_node->board) == NULL) {
+            addFirst(&closedList_p, cur_node);
+            for (int i = 0; i < MAX_BOARD; i++) {
+                child_p = getChildBoard(cur_node, i);
+                if (child_p != NULL && onList(&closedList_p, child_p->board) == NULL) {
+                    if (onList(&openList_p, child_p->board) == NULL) {
+                        child_p->h = getSimpleHeuristic(child_p);
+                        child_p->g = child_p->depth;
+                        child_p->f = child_p->g + child_p->h;
+                        addFirst(&openList_p, child_p);
+                    }
+                    else {
+                        temp = onList(&openList_p, child_p->board);
+                        if (temp->f > child_p->f) {
+                            addFirst(&openList_p, child_p);
+                        }
+                    }
+                }
+            }
         }
+    }
 
-		if (onList(&closedList_p, cur_node->board) == NULL) {
-			addFirst(&closedList_p, cur_node) ;
-			for (int i = 0; i < MAX_BOARD; i++){
-				child_p = getChildBoard(cur_node, i) ;
-				// it's a valid child and not visited
-				if (child_p != NULL && onList(&closedList_p,child_p->board)==NULL) { 
-					if(onList(&openList_p,child_p->board)==NULL){
-						// estimation optimiste (minorante) du coût de n à la solution
-						child_p->h = getSimpleHeuristic(child_p) ;
-						// coût pour aller de la racine jusqu’au nœud n
-						child_p->g = child_p->depth ;
-						// estimation du coût global 
-						child_p->f = child_p->g+child_p->h ;
-						
-						/* Add child node to openList */
-						addFirst(&openList_p, child_p) ;
-					}
-					else{
-						temp = onList(&openList_p, child_p->board) ;
-						// If the node already exists but with a higher cost
-						if(temp->f > child_p->f){
-							/* Add child node to openList */
-							addFirst(&openList_p, child_p) ;
-						}
-					}
-				}
-			}
-		}
-	}
-
+    return reachableNodes;
 }
+
 
 int main()
 { 
