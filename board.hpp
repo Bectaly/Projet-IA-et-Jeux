4d771567ff9d4177cb2fb34e75b03e9b319cd5cb
Bectaly <dorian.cadenel@laposte.net>
2024-05-07 09:00:53 +0200

    rtrg

---
+++ board.hpp
@@ -0,0 +1,27 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+typedef struct z_tile
+{
+    bool up;
+    bool right;
+    bool down;
+    bool left;
+    bool p1;
+    bool p2;
+} Tile;
+
+typedef Tile* Board;
+
+Board createBoard(int w, int h)
+Board initGame(Board board)
+
+void printBoard(Board board)
+void printBoardHumanReadable(Board board)
+
+void placeWall(Board board, int x, int y, int bit)
+void movePlayer(Board board, int id, int bit)
+
+void getTileData(Board board, int x, int y)
+
+#endif
